2391b6c138104027991874581887923af00adee6
susumu tomita <oyster880@gmail.com>
2024-02-12 13:59:04 +0900

    add eas

--- ExpoLab/apps/labapp/App.tsx
+++ ExpoLab/apps/labapp/App.tsx
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import * as MyModule from 'my-module'; // <-- 追加
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>{MyModule.hello()}</Text>{/* <-- 追加*/}
       <StatusBar style="auto" />
     </View>
   );
